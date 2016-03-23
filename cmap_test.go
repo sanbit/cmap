5dd74dec1d48805d973b9001cacd99fc30ff3e8b
liuweisan <liuweisan@126.com>
2016-03-23 20:00:41 +0800

    add tests

--- cmap_test.go
+++ cmap_test.go
@@ -27,3 +27,43 @@ func Test_map_bucket_negative(t *testing.T) {
 	NewConcurrentMap(-8)
 	t.Error("bucket<0 not panic")
 }
+
+func Test_get_exist(t *testing.T) {
+
+	m := NewConcurrentMap(5)
+
+	value := "aaa"
+	m.Set("aaa", value)
+	m.Set("aaa", value)
+
+	o := m.Get("aaa")
+
+	if o == nil {
+		t.Fatal("already set but can not get")
+	}
+
+	v, ok := o.(string)
+
+	if !ok {
+		t.Fatal("get a wrong type")
+	}
+
+	if v != value {
+		t.Fatal("get a wrong value !=", value)
+	}
+}
+
+func Test_get_nil(t *testing.T) {
+
+	m := NewConcurrentMap(5)
+
+	value := "aaa"
+	m.Set("aaa", value)
+	m.Set("aaa", value)
+
+	o := m.Get("bbb")
+
+	if o != nil {
+		t.Fatal("already set but can not get")
+	}
+}
